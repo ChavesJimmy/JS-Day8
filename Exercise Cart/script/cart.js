b1696268cc3881116c73eef9d985c668b3a4a115
Jimmy CHAVES <jimmybakerchaves@gmail.com>
2022-10-04 13:15:47 +0200

    basic shopping cart done

--- Exercise Cart/script/cart.js
+++ Exercise Cart/script/cart.js
@@ -69,3 +69,126 @@ for (let flower of flowers) {
            </div>
    `;
 }
+//product button selected
+const addToCartBtn = document.querySelectorAll( ".product-button");
+
+//add event to add to cart buttons
+addToCartBtn.forEach((btn, i) => {
+ btn.addEventListener("click" , () => {
+   addToCart(flowers[i]);
+ });
+});
+//cart declared
+
+const cart = [];
+
+//adds product to cart
+
+const addToCart = (flowers) => {
+  if (cart.find((val) => val.name == flowers.name)) {
+    // console.log(cart.find((val) => val.name == product.name));
+    flowers.qtty++;
+  } else {
+    cart.push(flowers);
+  }
+ // console.table(cart);
+ createRows();
+ cartTotal();
+ };
+
+ const createRows = () => {
+  let result = "";
+  for (let  item of cart) {
+    result += `
+    <div class="cart-row row gx-0">
+    <div class="cart-item col-6 col-sm-6 my-2 d-flex align-items-center justify-content-center">
+                        <img class="cart-item-image" src="${
+                          item.image
+                        }" width="100" height="100" alt="${item.name}">
+                        <div class="cart-item-title h5 ms-2">JavaScript Course</div>
+                    </div>
+                    <div class="cart-qtty-action col-2 d-flex justify-content-center align-items-center">
+                        <div class="d-flex">
+                            <i class="plus fs-5 bi bi-plus-circle-fill"></i>
+                        </div>
+                        <div class="text-center m-0 cart-quantity h4 w-25">${
+                          item.qtty
+                        }</div>
+                        <div class="d-flex">
+                            <i class="minus fs-5 bi bi-dash-circle-fill"></i>
+                        </div>
+                    </div>
+                    <div class="col-1 d-flex justify-content-start align-items-center">
+                    <i class="del fs-4 bi bi-trash3-fill text-danger"></i>
+                    </div>
+                    <div class="cart-price col-3 h5 my-auto text-end pe-5">${currencyFormater.format(
+                      item.price
+                    )}</div>
+                    <div class="number-items col-3 h5 my-auto text-end pe-5">${
+                      item.qtty
+                    }</div>
+                </div>                    
+                </div>
+        `;
+  }
+  document.querySelector(".cart-items" ).innerHTML = result;
+ const plusBtns = document.querySelectorAll(".plus");
+plusBtns.forEach((btn, i) => {
+   btn.addEventListener("click", () => {
+     plusQtty(i);
+   });
+ });
+ 
+const minusBtns = document.querySelectorAll(".minus");
+minusBtns.forEach((btn, i) => {
+   btn.addEventListener("click", () => {
+     minusQtty(i);
+   });
+ });
+ const deleteBtns = document .querySelectorAll(".del");
+ deleteBtns.forEach((btn, i) => {
+  btn.addEventListener("click", () => {
+    deleteItem(i);
+  });
+});
+};
+ //updates the cart total amount (+discount)
+const cartTotal = () => {
+  let total = 0;
+  for (let  item of cart) {
+    total += item.price * item.qtty;
+
+ /*    if(total >= 25){
+    total = (item.price * item.qtty)-((item.price * item.qtty)*0.1)
+  }
+    else {total += item.price * item.qtty;
+  } */}
+  
+  document.getElementById("price").innerHTML = currencyFormater.format(total);
+ };
+ 
+//increases item quantity
+const plusQtty = (index) => {
+  cart[index].qtty++;
+  createRows();
+  cartTotal();
+ };
+ 
+//decreases item quantity
+const minusQtty = (index) => {
+  if (cart[index].qtty == 1) {
+    cart.splice(index, 1);
+  } else {
+    cart[index].qtty--;
+  }
+  createRows();
+  cartTotal();
+ };
+ 
+//deletes item from cart
+const deleteItem = (index) => {
+  cart[index].qtty = 1;
+  cart.splice(index, 1);
+  createRows();
+  cartTotal();
+ };
